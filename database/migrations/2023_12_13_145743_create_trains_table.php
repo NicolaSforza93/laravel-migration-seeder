36f8e3a16f4c8cf10a7ddf8b75631a1a0f9ec669
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-12-13 16:36:46 +0100

    added columns table trains and launched migration

--- database/migrations/2023_12_13_145743_create_trains_table.php
+++ database/migrations/2023_12_13_145743_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 20);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
+            $table->unsignedSmallInteger('codice_treno')->unique();
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->unsignedTinyInteger('in_orario');
+            $table->unsignedTinyInteger('cancellato');
             $table->timestamps();
         });
     }
