006be928227439a21cbc158210027461beb915fe
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-12-14 17:04:54 +0100

    added trains with Faker

--- database/migrations/2023_12_13_145743_create_trains_table.php
+++ database/migrations/2023_12_13_145743_create_trains_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('azienda', 20);
+            $table->string('azienda', 50)->default('Trenitalia');
             $table->string('stazione_di_partenza', 50);
             $table->string('stazione_di_arrivo', 50);
             $table->dateTime('orario_di_partenza');
             $table->dateTime('orario_di_arrivo');
-            $table->unsignedSmallInteger('codice_treno')->unique();
+            $table->unsignedSmallInteger('codice_treno');
             $table->unsignedTinyInteger('numero_carrozze');
             $table->string('in_orario', 2);
             $table->string('cancellato', 2);
---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Train;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        // $faker = Faker\Factory::create('it_IT');
+        for ($i = 0; $i < 10; $i++) {
+            $new_train = new Train();
+
+            // $new_train->azienda = $faker->company();
+            $new_train->stazione_di_partenza = $faker->city();
+            $new_train->stazione_di_arrivo = $faker->city();
+            $new_train->orario_di_partenza = $faker->dateTimeBetween('-1 day', '+3 day');
+            $new_train->orario_di_arrivo = $faker->dateTimeBetween('-1 day', '+3 day');
+            $new_train->codice_treno = $faker->bothify('####');
+            $new_train->numero_carrozze = $faker->numberBetween(1, 11);
+            $new_train->in_orario = $faker->randomElement(['si', 'no']);
+            $new_train->cancellato = $faker->randomElement(['si', 'no']);
+
+            $new_train->save();
+        }
+    }
+}
